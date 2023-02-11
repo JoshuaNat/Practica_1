817ca825d3723f76ef4146195c8ee91a5363b105
Joshua <xyznathaniel@gmail.com>
2023-02-11 15:02:13 -0600

    Segunda estructura

--- P1_Lotes.py
+++ P1_Lotes.py
@@ -1,21 +1,37 @@
-class Datos:
+class DatosO:
 	"""Una clase simple para almacenar datos"""
 
-	def __init__(self, ipv6, nombre, apellido, correo, genero, ipv4):
+	def __init__(self, ipv6, apellido, ipv4):
 		"""Inicializa los atributos de la clase"""
 		self.ipv6 = ipv6
-		self.nombre = nombre
 		self.apellido = apellido
-		self.correo = correo
-		self.genero = genero
 		self.ipv4 = ipv4
+
+class DatosF:
+	"""Clase para almacenar los datos de salida"""
+
+	def __init__(self, NumH, apellido, NumD):
+		"""Inicializa los atributos de la clase"""
+		self.NumH = NumH
+		self.apellido = apellido
+		self.NumD = NumD
+
+
+#Lista de datos
+Datos_Originales = []
+Datos_Finales = []
 
 def contenido_archivo(filename):
 	"""Abre y cierra el archivo de texto"""
 	with open(filename) as lotes:
 		for line in lotes: 
 			info = line.split(",") #Tras leerlo linea por linea, separa las lineas por comas
-			mis_datos = Datos(info[0], info[1], info[2], info[3], info[4], info[5]) #Se asignan los valores correspondientes a los atributos de la clase
+			Datos_Originales.append(DatosO(info[0], info[2], info[5])) #Se asignan los valores y se anexan a la lista
+
+def hex_a_dec():
+	for obj in Datos_Originales:
+		H = obj.ipv6.split(":")
 
 
 contenido_archivo("prueba2.txt")
+hex_a_dec()
