268169f9dc78faead51fb4f054c98327d391e7df
Joshua <xyznathaniel@gmail.com>
2023-02-11 14:32:48 -0600

    asignacion de atributos

--- P1_Lotes.py
+++ P1_Lotes.py
@@ -1,7 +1,7 @@
-class datos:
+class Datos:
 	"""Una clase simple para almacenar datos"""
 
-	def __init__(ipv6, nombre, apellido, correo, genero, ipv4):
+	def __init__(self, ipv6, nombre, apellido, correo, genero, ipv4):
 		"""Inicializa los atributos de la clase"""
 		self.ipv6 = ipv6
 		self.nombre = nombre
@@ -15,7 +15,7 @@ def contenido_archivo(filename):
 	with open(filename) as lotes:
 		for line in lotes: 
 			info = line.split(",") #Tras leerlo linea por linea, separa las lineas por comas
-			
+			mis_datos = Datos(info[0], info[1], info[2], info[3], info[4], info[5]) #Se asignan los valores correspondientes a los atributos de la clase
 
 
 contenido_archivo("prueba2.txt")
