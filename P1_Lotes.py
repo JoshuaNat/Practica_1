4d02288786df453342acd0a916eb92e9e20c69aa
Joshua <xyznathaniel@gmail.com>
2023-02-12 13:39:02 -0600

    simplificacion codigo

--- P1_Lotes.py
+++ P1_Lotes.py
@@ -1,4 +1,4 @@
-class DatosO:
+class Datos:
 	"""Una clase simple para almacenar datos"""
 
 	def __init__(self, ipv6, apellido, ipv4):
@@ -7,29 +7,20 @@ class DatosO:
 		self.apellido = apellido
 		self.ipv4 = ipv4
 
-class DatosF:
-	"""Clase para almacenar los datos de salida"""
-
-	def __init__(self, NumH, apellido, NumD):
-		"""Inicializa los atributos de la clase"""
-		self.NumH = NumH
-		self.apellido = apellido
-		self.NumD = NumD
-
 
 #Lista de datos
-Datos_Originales = []
-Datos_Finales = []
+list = []
+
 
 def contenido_archivo(filename):
 	"""Abre y cierra el archivo de texto"""
 	with open(filename) as lotes:
 		for line in lotes: 
 			info = line.split(",") #Tras leerlo linea por linea, separa las lineas por comas
-			Datos_Originales.append(DatosO(info[0], info[2], info[5])) #Se asignan los valores y se anexan a la lista
+			list.append(Datos(info[0], info[2], info[5])) #Se asignan los valores y se anexan a la lista
 
 def hex_a_dec():
-	for obj in Datos_Originales:
+	for obj in list:
 		H = obj.ipv6.split(":")
 		for numero in H:
 			D = int(numero, base=16)
